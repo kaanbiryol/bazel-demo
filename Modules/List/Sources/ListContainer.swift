5e4e71e72e335aade4b5ea7595cd12844eb4f744
Kaan Biryol <dev.biryolkaan@gmail.com>
2024-05-29 15:54:32 +0200

    Create ListContainer.swift

---
+++ Modules/List/Sources/ListContainer.swift
@@ -0,0 +1,19 @@
+import Foundation
+import SwiftUI
+import NetworkingInterface
+
+private struct ListContainer: View {
+    private let networkingService: any NetworkingService
+
+    init(networkingService: any NetworkingService) {
+        self.networkingService = networkingService
+    }
+
+    var body: some View {
+        ListView(networkingService: Mock())
+    }
+}
+
+private class TestPrivateClass {
+    private var testPrivateParam: String = ""
+}
